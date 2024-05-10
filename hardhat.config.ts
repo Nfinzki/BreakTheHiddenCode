9ed76ed5ff91be8f1a1c7d0bc5006ba045fbb40b
Francesco Di Luzio <francescodiluzio07@gmail.com>
2024-05-10 20:09:07 +0200

    Added gasReporter dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  gasReporter: {
+    enabled: true,
+    noColors: false,
+    currency: "EUR",
+    outputFile: "gas-report-matic.txt",
+    token: "MATIC",
+  },
 };
 
 export default config;
